19540d4fe666690b61f67b111c91e47dc61c02d8
tpruthvi <44976743+tpruthvi@users.noreply.github.com>
2019-03-29 09:10:52 +0530

    Create calc_header.h

---
+++ calc_header.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+#include <assert.h>
+#include<math.h>
+
+int addition(int x,int y);
+int subtraction(int x,int y);
+int multiplication(int x,int y);
+int division(int x,int y);
+int power(int x,int y);
+void test_addition(void);
+void test_subtraction(void);
+void test_multiplication(void);
+void test_division(void);
+void test_power(void);
