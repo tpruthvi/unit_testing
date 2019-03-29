452dd80e36aa196b4f891e64870e4f7d162b9cee
tpruthvi <44976743+tpruthvi@users.noreply.github.com>
2019-03-29 09:10:09 +0530

    Create calc_fn.c

---
+++ calc_fn.c
@@ -0,0 +1,21 @@
+#include "test_cases.h"
+int addition(int x,int y)
+{
+    return x+y;
+}
+int subtraction(int x,int y)
+{
+    return x-y;
+}
+int multiplication(int x,int y)
+{
+    return x*y;
+}
+int division(int x,int y)
+{
+    return x/y;
+}
+int power(int x,int y)
+{
+    return pow(x,y);
+}
